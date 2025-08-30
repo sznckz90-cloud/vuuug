fdc9534e455edc402fbd5994877d17343370a26b
sznckz90-cloud <sznckz90@gmail.com>
2025-08-31 03:12:58 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
