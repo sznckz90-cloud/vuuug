560513cb9dd6976e93a2b3b6f9de799a51f1d218
sznckz90-cloud <sznckz90@gmail.com>
2025-09-04 20:42:11 +0530

    Add files via upload

---
+++ client/src/types/telegram.d.ts
@@ -0,0 +1,28 @@
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        initData: string;
+        initDataUnsafe: {
+          query_id?: string;
+          user?: {
+            id: number;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+            is_premium?: boolean;
+            photo_url?: string;
+          };
+          auth_date: number;
+          hash: string;
+        };
+        ready: () => void;
+        expand: () => void;
+        close: () => void;
+      };
+    };
+  }
+}
+
+export {};
