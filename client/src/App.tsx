1d29a249b18b822c827c90ebeb183b4332c2dadc
sznckz90-cloud <sznckz90@gmail.com>
2025-09-14 14:49:53 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,24 +7,16 @@ import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Tasks from "@/pages/Tasks";
 import Profile from "@/pages/Profile";
-import Promote from "@/pages/Promote";
 import Admin from "@/pages/Admin";
-import CreateTask from "@/pages/CreateTask";
-import MyPromotions from "@/pages/MyPromotions";
 import Wallet from "@/pages/Wallet";
-import Affiliates from "@/pages/Affiliates";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/tasks" component={Tasks} />
-      <Route path="/create-task" component={CreateTask} />
-      <Route path="/my-promotions" component={MyPromotions} />
       <Route path="/wallet" component={Wallet} />
-      <Route path="/affiliates" component={Affiliates} />
       <Route path="/profile" component={Profile} />
-      <Route path="/promote" component={Promote} />
       <Route path="/admin" component={Admin} />
       <Route component={NotFound} />
     </Switch>
