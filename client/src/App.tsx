5caadc67b960dbcf93d158368af2700b914ff35b
sznckz90-cloud <sznckz90@gmail.com>
2025-10-07 17:49:32 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import AppNotification from "@/components/AppNotification";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Tasks from "@/pages/Tasks";
+import Spin from "@/pages/Spin";
 import Landing from "@/pages/Landing";
 import Admin from "@/pages/Admin";
 import Wallet from "@/pages/Wallet";
@@ -16,6 +17,7 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/tasks" component={Tasks} />
+      <Route path="/spin" component={Spin} />
       <Route path="/wallet" component={Wallet} />
       <Route path="/affiliates" component={Affiliates} />
       <Route path="/profile" component={Landing} />
