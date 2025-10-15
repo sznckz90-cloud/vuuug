90d077d2ca17fe542ab70ac508e8f6d295e914af
sznckz90-cloud <sznckz90@gmail.com>
2025-10-15 15:53:07 +0530

    Delete client/src/components/GhostLogo.tsx

--- client/src/components/GhostLogo.tsx
+++
@@ -1,9 +0,0 @@
-const ghostLogo = '/ghost-logo.jpg';
-
-export default function GhostLogo() {
-  return (
-    <div className="w-10 h-10 rounded-full overflow-hidden">
-      <img src={ghostLogo} alt="👻" className="w-full h-full object-cover" />
-    </div>
-  );
-}
