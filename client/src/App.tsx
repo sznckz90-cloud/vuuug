8ed70d2986c1a8502acc38923d4e93d81149fa2f
sznckz90-cloud <sznckz90@gmail.com>
2025-10-31 23:13:08 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import AppNotification from "@/components/AppNotification";
 import { useEffect, lazy, Suspense, useState } from "react";
 import { setupDeviceTracking } from "@/lib/deviceId";
 import BanScreen from "@/components/BanScreen";
+import SeasonEndOverlay from "@/components/SeasonEndOverlay";
 
 const Home = lazy(() => import("@/pages/Home"));
 const Landing = lazy(() => import("@/pages/Landing"));
@@ -42,6 +43,7 @@ function Router() {
 function App() {
   const [isBanned, setIsBanned] = useState(false);
   const [banReason, setBanReason] = useState<string>();
+  const [showSeasonEnd, setShowSeasonEnd] = useState(false);
 
   useEffect(() => {
     const tg = window.Telegram?.WebApp;
@@ -87,7 +89,26 @@ function App() {
         })
         .catch(() => {});
     }
+
+    fetch("/api/app-settings")
+      .then(res => res.json())
+      .then(settings => {
+        if (settings.seasonBroadcastActive) {
+          const hasSeenSeasonEnd = localStorage.getItem("season_end_seen");
+          if (!hasSeenSeasonEnd) {
+            setShowSeasonEnd(true);
+          }
+        } else {
+          localStorage.removeItem("season_end_seen");
+        }
+      })
+      .catch(() => {});
   }, []);
+
+  const handleCloseSeasonEnd = () => {
+    localStorage.setItem("season_end_seen", "true");
+    setShowSeasonEnd(false);
+  };
 
   if (isBanned) {
     return <BanScreen reason={banReason} />;
@@ -97,6 +118,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <AppNotification />
+        {showSeasonEnd && <SeasonEndOverlay onClose={handleCloseSeasonEnd} />}
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
