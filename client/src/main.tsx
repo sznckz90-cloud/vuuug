2c0ce2f84ffeff530270a174525adcfd8af3a6ad
sznckz90-cloud <sznckz90@gmail.com>
2025-11-09 16:49:00 +0530

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Buffer } from "buffer";
-
-window.Buffer = Buffer;
 
 document.addEventListener("contextmenu", (e) => e.preventDefault());
 
